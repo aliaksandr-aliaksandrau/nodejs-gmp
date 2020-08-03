e3f78988e57161d3805bb745ee35ae259a44d649
Aliaksandr Aliaksandrau1 <aliaksandr_aliaksandrau1@epam.com>
2020-08-03 15:34:38 +0300

    initial commit

---
+++ src/task1.ts
@@ -0,0 +1,10 @@
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin
+});
+
+rl.on('line', (line: string) => {
+  console.log(line.split('').reverse().join(''));
+});

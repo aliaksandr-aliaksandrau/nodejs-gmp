7851abfe734c553cd8b49fccfcf6f405b89b594b
Aleksandr Aleksandrov <Aliaksandr_Aliaksandrau1@epam.com>
2020-08-30 15:33:51 +0300

    feat: add eslint and fix code

--- 02-crud-rest-service/src/app.ts
+++ 02-crud-rest-service/src/app.ts
@@ -16,73 +16,71 @@ mockUsers.forEach((user: User) => users.set(user.id, user));
 
 
 app.listen(port, () => {
-  console.log(`App is running...`)
+    console.log('App is running...');
 });
 
 app.use(express.json());
 
 router.param('id', (req: any, res, next: NextFunction, id) => {
-
-  req.user = users.get(id);
-
-  next();
-})
+    req.user = users.get(id);
+    next();
+});
 
 router.get('/users/:id', (req: any, res, next: NextFunction) => {
-  const user = req.user;
-  user ? res.json(user) : responseUserNotFoundHandler(res);
-  next();
+    const user = req.user;
+    user ? res.json(user) : responseUserNotFoundHandler(res);
+    next();
 });
 
 router.delete('/users/:id', (req: any, res, next: NextFunction) => {
-  const user = req.user;
+    const user = req.user;
 
-  if (!!user) {
-    user.isDeleated = true;
-    res.json(`User ${user.id} was deleted`);
-  } else {
-    responseUserNotFoundHandler(res);
-  }
+    if (!!user) {
+        user.isDeleated = true;
+        res.json(`User ${user.id} was deleted`);
+    } else {
+        responseUserNotFoundHandler(res);
+    }
 
-  next();
+    next();
 });
 
 router.put('/users', (req, res, next: NextFunction) => {
-  const user = req.body.user as User;
+    const user = req.body.user as User;
 
-  if (user && users.has(user.id)) {
-    users.set(user.id, user)
-    res.json(`User ${user.id} was updated to ${JSON.stringify(user)}`);
-  } else {
-    responseUserNotFoundHandler(res);
-  }
+    if (user && users.has(user.id)) {
+        users.set(user.id, user);
+        res.json(`User ${user.id} was updated to ${JSON.stringify(user)}`);
+    } else {
+        responseUserNotFoundHandler(res);
+    }
 
-  next();
+    next();
 });
 
 router.post('/users', (req, res, next) => {
-  const user = req.body.user as User;
+    const user = req.body.user as User;
 
-  if (user) {
-    users.set(user.id, user);
-    res.json(`User was created ${JSON.stringify(user)}`);
-  }
+    if (user) {
+        users.set(user.id, user);
+        res.json(`User was created ${JSON.stringify(user)}`);
+    }
 
-  next();
+    next();
 });
 
 router.use('/suggested-users', (req, res, next) => {
-  const substr = req.query.loginSubstring?.toString();
-  const limit = Number.parseInt(req.query.limit?.toString() as string);
+    const substr = req.query.loginSubstring?.toString();
+    const limit = Number.parseInt(req.query.limit?.toString() as string, 10);
 
-  if (substr && !isNaN(limit)) {
-    const suggestedUsers = getAutoSuggestUsers([...users.values()], substr, limit);
-    res.json(`Suggested users ${JSON.stringify(suggestedUsers)}`);
-  } else {
-    res.status(400).json('Please enter correct parameters');
-  }
+    if (substr && !isNaN(limit)) {
+        const suggestedUsers = getAutoSuggestUsers([...users.values()], substr, limit);
+        res.json(`Suggested users ${JSON.stringify(suggestedUsers)}`);
+    } else {
+        res.status(400).json('Please enter correct parameters');
+    }
 
-  next();
+    next();
 });
 
 app.use('/', router);
--- 02-crud-rest-service/src/utility.ts
+++ 02-crud-rest-service/src/utility.ts
@@ -1,11 +1,11 @@
-import { User } from "./model/user";
+import { User } from './model/user';
 
 export function responseUserNotFoundHandler(res: any): void {
-    res.status(404).json(`User was not found`);
+    res.status(404).json('User was not found');
 }
 
 export function getAutoSuggestUsers(users: User[], loginSubstring: string, limit: number): User[] {
-    //TODO add sorting 
+    // TODO add sorting
 
     const filteredUsers = users
         .filter(user => user.login.includes(loginSubstring))

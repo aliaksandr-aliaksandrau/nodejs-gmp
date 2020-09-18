34cae2bc326e4b0d0655e9ae664dc32e511ef106
Aliaksandr Aliaksandrau1 <aliaksandr_aliaksandrau1@epam.com>
2020-09-18 20:27:36 +0300

    feat(postgres): add sequelize

--- 03-layered-architecture/src/app.ts
+++ 03-layered-architecture/src/app.ts
@@ -1,11 +1,47 @@
 import express from 'express';
 import { Express } from 'express';
+import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
+import { sequilize as sq } from './database/connections';
 
-import { createRouter } from './server-router/router';
+const sequilize: Sequelize = sq;
 
-const app: Express = express();
-const port: number = 3500;
+// const { Client } = require('pg');
+// const connectionString = 'postgres://glbzjpvy:bR9HhMlTqv16LOPM_iR_RiU_yQdmLg-W@balarama.db.elephantsql.com:5432/glbzjpvy';
+// const client = new Client({
+//     connectionString
+// });
+// client.connect();
 
-app.listen(port, () => console.log('App is running...'));
-app.use(express.json());
-app.use('/', createRouter());
+const app = express();
+app.set('port', process.env.PORT || 4000);
+// app.get('/', (req, res, next) => {
+//     client.query('SELECT * FROM users', (err: any, result: { rows: any; }) => {
+//         if (err) {
+//             console.log(err);
+//             res.status(400).send(err);
+//         }
+//         res.status(200).send(result.rows);
+//         console.log('Data: ', result.rows);
+//     });
+// });
+app.listen(4000, () => {
+    console.log('Server is running.. on Port 4000');
+});
+
+sequilize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+const User = sequilize.define('user', {
+    id: DataTypes.INTEGER,
+    login: DataTypes.STRING,
+    password: DataTypes.STRING,
+    age: DataTypes.NUMBER,
+    isDeleated: DataTypes.BOOLEAN
+});
+

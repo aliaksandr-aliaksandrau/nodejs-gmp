bd37e190c2c141195f62af466b949ec5faebd1e6
Aliaksandr Aliaksandrau1 <aliaksandr_aliaksandrau1@epam.com>
2020-09-22 19:37:34 +0300

    feat(sequilize): add user model

--- 03-layered-architecture/src/app.ts
+++ 03-layered-architecture/src/app.ts
@@ -1,29 +1,13 @@
 import express from 'express';
 import { Express } from 'express';
-import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
+import { Sequelize, DataTypes, Model, Optional, ModelCtor } from 'sequelize';
 import { sequilize as sq } from './database/connections';
 
 const sequilize: Sequelize = sq;
 
-// const { Client } = require('pg');
-// const connectionString = 'postgres://glbzjpvy:bR9HhMlTqv16LOPM_iR_RiU_yQdmLg-W@balarama.db.elephantsql.com:5432/glbzjpvy';
-// const client = new Client({
-//     connectionString
-// });
-// client.connect();
-
 const app = express();
 app.set('port', process.env.PORT || 4000);
-// app.get('/', (req, res, next) => {
-//     client.query('SELECT * FROM users', (err: any, result: { rows: any; }) => {
-//         if (err) {
-//             console.log(err);
-//             res.status(400).send(err);
-//         }
-//         res.status(200).send(result.rows);
-//         console.log('Data: ', result.rows);
-//     });
-// });
+
 app.listen(4000, () => {
     console.log('Server is running.. on Port 4000');
 });
@@ -37,11 +21,40 @@ sequilize
         console.error('Unable to connect to the database:', err);
     });
 
-const User = sequilize.define('user', {
-    id: DataTypes.INTEGER,
-    login: DataTypes.STRING,
-    password: DataTypes.STRING,
-    age: DataTypes.NUMBER,
-    isDeleated: DataTypes.BOOLEAN
-});
+export const UserModel: ModelCtor<Model> = sequilize.define(
+    'users',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        deleated: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+    },
+    {
+        timestamps: false
+    }
+);
 
+// UserModel.findAll({ limit: 10 }).then((d) => {
+//     console.log('AAA: USers: ', d);
+// });
+
+UserModel.findByPk(1).then((d) => {
+    console.log('AAA: USers: ', d?.toJSON());
+});

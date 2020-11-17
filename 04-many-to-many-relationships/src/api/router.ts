9bf04c81ea6404266fe12532b4ff3e0bafdab8e7
Aliaksandr Aliaksandrau1 <aliaksandr_aliaksandrau1@epam.com>
2020-11-17 18:10:51 +0300

    fix: user and group validation schemas

--- 04-many-to-many-relationships/src/api/router.ts
+++ 04-many-to-many-relationships/src/api/router.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
 
-import { userSchema, groupSchema, validate } from '../data/validation';
+import {
+    validate,
+    userUpdateSchema,
+    userCreateSchema,
+    groupUpdateSchema,
+    groupCreateSchema
+} from '../data/validation';
 import { GroupService } from '../services/group-service';
 import { UserService } from '../services';
 
@@ -15,15 +21,27 @@ export function createRouter(): Router {
             .get('/users/:id', userService.getUser)
             .get('/users', userService.getAllUsers)
             .delete('/users/:id', userService.deleteUser)
-            .put('/users/:id', validate(userSchema), userService.updateUser)
-            .post('/users', validate(userSchema), userService.createUser)
+            .put(
+                '/users/:id',
+                validate(userUpdateSchema),
+                userService.updateUser
+            )
+            .post('/users', validate(userCreateSchema), userService.createUser)
             .use('/users/suggested-users', userService.getSuggestedUsers)
             // group
             .get('/groups', groupService.getAllGroups)
             .get('/groups/:id', groupService.getGroupById)
             .delete('/groups/:id', groupService.deleteGroup)
-            .put('/groups/:id', validate(groupSchema), groupService.updateGroup)
-            .post('/groups', validate(groupSchema), groupService.createGroup)
+            .put(
+                '/groups/:id',
+                validate(groupUpdateSchema),
+                groupService.updateGroup
+            )
+            .post(
+                '/groups',
+                validate(groupCreateSchema),
+                groupService.createGroup
+            )
             // user groups
             .post('/groups/add-users', groupService.addUsersToGroup)
             .get('/groups/users/:id', groupService.getUsersByGroupId)
--- 04-many-to-many-relationships/src/data/validation/group-schema.ts
+++ 04-many-to-many-relationships/src/data/validation/group-schema.ts
@@ -1,13 +1,16 @@
 import Joi, { string, ObjectSchema } from 'joi';
 
-export const groupSchema: ObjectSchema = Joi.object().keys({
+const nameRegExp: RegExp = /^[a-zA-Z0-9]{3,30}$/;
+const getPermissionsSchema = () =>
+    Joi.array().items('READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES');
+
+export const groupUpdateSchema: ObjectSchema = Joi.object().keys({
     id: Joi.string().optional(),
-    name: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
-    permissions: Joi.array().items(
-        'READ',
-        'WRITE',
-        'DELETE',
-        'SHARE',
-        'UPLOAD_FILES'
-    )
+    name: Joi.string().regex(nameRegExp).optional(),
+    permissions: getPermissionsSchema().optional()
+});
+
+export const groupCreateSchema: ObjectSchema = Joi.object().keys({
+    name: Joi.string().regex(nameRegExp).required(),
+    permissions: getPermissionsSchema().required()
 });
